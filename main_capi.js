10c69b9244a57aec401449fd7bf58bebfd5276c2
Qubayl90 <Qubayl90@gmail.com>
2023-04-29 14:02:34 +0530

    Add files via upload

--- main_capi.js
+++ main_capi.js
@@ -4,7 +4,6 @@ let img_artist_title;
 let img_artwork_type_title;
 let img_department_title;
 
-
  async function clickedEvent(img_id){
     let request = new Request(`https://api.artic.edu/api/v1/artworks/${img_id}`,{
          method: 'GET',
@@ -13,6 +12,14 @@ let img_department_title;
 
      let result = await fetch(request);
      let response = await result.json();
+    
+     console.log('API Response:');
+     console.log(response);
+     console.log(`Art Id: ${response.data.id}`);
+     console.log(`Art Title: ${response.data.title}`);
+     console.log(`Artist Title: ${response.data.artist_title}`);
+     console.log(`Artwork Type: ${response.data.artwork_type_title}`);
+     console.log(`Department Title: ${response.data.department_title}`);
 
      document.getElementById('art_info').style.display = 'block';
      document.getElementById("art_id").innerHTML = response.data.id;
@@ -31,9 +38,7 @@ let img_department_title;
      document.getElementById(img_id).style.borderWidth ='7px';
      document.getElementById(img_id).style.borderColor ='Black';
      window.scrollTo(0, document.body.scrollHeight);
-     
- }
-
+}
 
 function getImage(img_id,event){
     event.stopPropagation();
